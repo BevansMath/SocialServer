d1aaf7cd2939ed1a8d3df8a38f4883d2eebb4ef2
root <root@DESKTOP-SJMT7AE.localdomain>
2022-07-22 08:43:48 -0500

    Updated sandbox-main

--- sandbox/sandbox-main.go
+++ sandbox/sandbox-main.go
@@ -13,8 +13,42 @@ func main() {
 	err := c.EnsureDB
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(1) // upon testing this code, an exit code of one was given.
+
+	}
+	user, err := c.CreateUser("test@example.com", "password", "Jane Doe", 18)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	fmt.Println("user created", user)
+
+	updatedUser, err := c.UpdatedUser("test@example.com", "password", "jane doe", 29)
+	if err != nil {
+		log.Fatal(err)
 		os.Exit(1)
 
 	}
-	fmt.Println("database ensured!")
+	fmt.Println("user updated", updatedUser)
+
+	gotUser, err := c.GetUser("test@example.com")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Println("got user", gotUser)
+
+	err = c.DeleteUser("test@example.com")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Println("user deleted")
+
+	_, err = c.GetUser("test@example.com")
+	if err == nil {
+		log.Fatal("User not retrieved")
+
+	}
+	fmt.Println("Confirmed removal of user...")
 }
